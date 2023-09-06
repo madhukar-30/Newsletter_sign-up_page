5b59f5491c0ae3b64de4443a61e32ca5fceef772
master-manas <manasmadhukar30@gmail.com>
2023-09-06 16:44:13 +0530

    improved

--- script.js
+++ script.js
@@ -1,74 +1,51 @@
+const inputEmail = document.querySelector("form input[type='email']");
+const submitInput = document.querySelector("form input[type='submit']");
+const errorSpan = document.getElementById("error");
+const loader = document.querySelector(".loader");
+const overlay = document.querySelector(".overlay");
+const newsletterForm = document.getElementById("newsletterForm");
 
-    const inputEmail = document.querySelector("form input[type='email']");
-    const submitInput = document.querySelector("form input[type='submit']");
-    const errorSpan = document.getElementById("error");
-    const loader = document.querySelector(".fa-solid");
-    const overlay = document.querySelector(".overlay");
-    const newsletterForm = document.getElementById("newsletterForm");
+inputEmail.addEventListener("focus", () => {
+  errorSpan.textContent = "";
+  inputEmail.style.border = "3px dotted rgb(86, 170, 209)";
+  inputEmail.style.outline = "none";
+});
 
-    const url ="thankyou.html";
-  
-    inputEmail.addEventListener("focus", () => {
-      errorSpan.textContent = "";
-      inputEmail.style.border = "3px dotted rgb(86, 170, 209)";
-      inputEmail.style.outline = "none";
-    });
-  
-    submitInput.addEventListener("click", (e) => {
-      let submit = false;
-      const emailValue = inputEmail.value.trim();
-      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+/;
-      inputEmail.style.borderColor = "red";
-  
-      if (emailValue === "") {
-        errorSpan.textContent = "Please fill in your email";
-      } else if (!emailPattern.test(emailValue)) {
-        errorSpan.textContent = "Valid email required";
-      } else {
-        errorSpan.textContent = "";
-        inputEmail.style.border = "1px solid hsl(231, 7%, 60%)";
-        submit = true;
-      }
-  
-      if (submit) {
+submitInput.addEventListener("click", (e) => {
+  e.preventDefault(); 
 
-        showLoader();
-        const formData = new FormData(newsletterForm);
-          setTimeout(() => {
-          
-            fetch("thankyou.html",{
-              method:'POST',
-              body: formData,
-              headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-                 
-                    }
-             
-          }).then((response)=>{
-            if(response.ok){
-              window.location.href="thankyou.html"; 
-            }
-            else{
-              console.error("Form submission failed.");
-            }
-          }).catch((error) => {
-            // Handle network errors
-            console.error("Network error:", error);
-          })
-          },2000 );
-       
-      } 
-    });
+  let submit = false;
+  const emailValue = inputEmail.value.trim();
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+/;
+  inputEmail.style.borderColor = "red";
 
-  
-function showLoader(){
-   
-    loader.style.visibility ="visible";
-    overlay.style.visibility ="visible";
+  if (emailValue === "") {
+    errorSpan.textContent = "Please fill in your email";
+  } else if (!emailPattern.test(emailValue)) {
+    errorSpan.textContent = "Valid email required";
+  } else {
+    errorSpan.textContent = "";
+    inputEmail.style.border = "1px solid hsl(231, 7%, 60%)";
+    submit = true;
+  }
+
+  if (submit) {
+    showLoader();
     
-}
-function hideLoader(){
-    loader.style.visibility ="hidden";
-    overlay.style.visibility ="hidden";
+    setTimeout(() => {
+      window.location.href = `thankyou.html?email=${encodeURIComponent(emailValue)}`;
+       hideLoader();
+    }, 900); 
+  
+  }
+});
+
+function showLoader() {
+  loader.style.visibility = "visible";
+  overlay.style.visibility = "visible";
 }
 
+function hideLoader() {
+  loader.style.visibility = "hidden";
+  overlay.style.visibility = "hidden";
+}
--- thankyou.js
+++ thankyou.js
@@ -5,7 +5,6 @@ const urlParams = new URLSearchParams(queryString);
 // Get the email query parameter
 const email = urlParams.get("email");
 
-// Now you can use the 'email' variable to display the email on the "thank you" page
 if (email) {
     // Display the email on the page
     document.getElementById("emailPlace").textContent = email;
